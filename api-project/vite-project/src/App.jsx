45929f2809c6bf2281b53c882b41c7769ddd37a1
Swapnil Ingale <ingaleswapnil1602@gmail.com>
2026-02-20 19:46:05 +0530

    pin functionality

--- api-project/vite-project/src/App.jsx
+++ api-project/vite-project/src/App.jsx
@@ -8,6 +8,30 @@ function App() {
   const deleteUser = (id) => {
     setUsers(users.filter(user => user.id !== id))
 
+  } 
+
+  const pinUser = (id) => {
+    // setUsers(users.map (user => user.id === id || user[0] )) 
+
+  //   const userToPin = users.find(user => user.id === id);
+  
+  // // Step 2: Create new array WITHOUT that user
+  // const remainingUsers = users.filter(user => user.id !== id);
+  
+  // // Step 3: Create new array WITH pinned user at start
+  // const updatedUsers = [userToPin, ...remainingUsers];
+  
+  // // Step 4: Update state
+  // setUsers(updatedUsers) 
+
+   const userToPin = users.find(user => user.id === id) 
+   console.log(userToPin) 
+
+   const remeaningUsers = users.filter(user => user.id !== id) 
+   console.log(remeaningUsers) 
+
+   setUsers([userToPin , ...remeaningUsers])
+
   }
 
   useEffect(() => {
@@ -39,6 +63,7 @@ function App() {
           {users.map(user => <div>
             <img key={user.id} src={user.avatar_url} style={{ height: 200, width: 200, border: "2px solid red" }} ></img>
             <button style={{ border: "2px solid black" }} onClick={() => deleteUser(user.id)}>delete{user.id}</button>
+            <button style={{ border: "2px solid black" }} onClick={() => pinUser(user.id)}>Pin</button>
           </div>)}
 
         </div>
