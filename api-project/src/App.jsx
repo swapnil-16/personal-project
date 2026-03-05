2e01d5808a95944210ca7ca582708c5f3c2f3229
Swapnil Ingale <ingaleswapnil1602@gmail.com>
2026-03-05 17:30:55 +0530

    clear all functionality

--- api-project/src/App.jsx
+++ api-project/src/App.jsx
@@ -5,32 +5,34 @@ function App() {
 
   const [users, setUsers] = useState([])
 
+  const [num, SetNum] = useState(30)
+
   const deleteUser = (id) => {
     setUsers(users.filter(user => user.id !== id))
 
-  } 
+  }
 
   const pinUser = (id) => {
     // setUsers(users.map (user => user.id === id || user[0] )) 
 
-  //   const userToPin = users.find(user => user.id === id);
-  
-  // // Step 2: Create new array WITHOUT that user
-  // const remainingUsers = users.filter(user => user.id !== id);
-  
-  // // Step 3: Create new array WITH pinned user at start
-  // const updatedUsers = [userToPin, ...remainingUsers];
-  
-  // // Step 4: Update state
-  // setUsers(updatedUsers) 
+    //   const userToPin = users.find(user => user.id === id);
 
-   const userToPin = users.filter(user => user.id === id) 
-   console.log(userToPin) 
+    // // Step 2: Create new array WITHOUT that user
+    // const remainingUsers = users.filter(user => user.id !== id);
 
-   const remeaningUsers = users.filter(user => user.id !== id) 
-   console.log(remeaningUsers) 
+    // // Step 3: Create new array WITH pinned user at start
+    // const updatedUsers = [userToPin, ...remainingUsers];
 
-   setUsers([...userToPin , ...remeaningUsers])
+    // // Step 4: Update state
+    // setUsers(updatedUsers) 
+
+    const userToPin = users.filter(user => user.id === id)
+    console.log(userToPin)
+
+    const remeaningUsers = users.filter(user => user.id !== id)
+    console.log(remeaningUsers)
+
+    setUsers([...userToPin, ...remeaningUsers])
 
   }
 
@@ -39,7 +41,7 @@ function App() {
 
     async function gitubUsers() {
 
-      const responce = await fetch("https://api.github.com/users")
+      const responce = await fetch(`https://api.github.com/users?per_page=${num}`)
 
       const data = await responce.json();
       setUsers(data)
@@ -49,25 +51,46 @@ function App() {
 
 
 
-  }, [])
+  }, [num])
 
+  // const handleEnteredUser = () => { 
+  //   console.log("the number has captured") 
+  //   SetNum(num)
 
+  // } 
+
+  const clearAll = () => {
+    // setUsers("");
+    setUsers([])
+  }
 
   return (
     <>
       <h1>The git hub users</h1>
+      <p className="m-4">Enter number of users to be rendered</p>
+      <input className="m-4" type="number " value={num} onChange={(e) => SetNum(e.target.value)}></input>
 
+      <div>
+        <button className="btn btn-warning m-2" onClick={clearAll}>clear all</button>
+      </div>
       <div style={{ display: "flex", justifyContent: "center", alignItems: "center", flexWrap: "wrap", gap: 20 }}>
 
         <div style={{ display: "flex", justifyContent: "center", alignItems: "center", flexWrap: "wrap", gap: 20 }}>
-          {users.map(user => <div>
-            <img key={user.id} src={user.avatar_url} style={{ height: 200, width: 200, border: "2px solid red" }} ></img>
-            <button style={{ border: "2px solid black" }} onClick={() => deleteUser(user.id)}>delete{user.id}</button>
-            <button style={{ border: "2px solid black" }} onClick={() => pinUser(user.id)}>Pin</button>
+          {users.map(user => <div className="card">
+            <img key={user.id} src={user.avatar_url} style={{ height: 150, width: 150, }} ></img>
+            <div className="#">
+              <p className="text-center fw-semibold mb-0">{user.login}</p>
+              <button className="btn btn-outline-danger m-2" onClick={() => deleteUser(user.id)}>Delete</button>
+              <button className="btn btn-outline-info" onClick={() => pinUser(user.id)}>Pin</button>
+            </div>
+
           </div>)}
 
         </div>
       </div>
+
+
+
 
 
 
